b2480269cdd899095d4c7bf9ac1e31e565cbaabb
mansi <mansi.valani@gmail.com>
2024-06-23 14:57:41 +0530

    Added config server with GIT Repo ref for properties and registered as eureka client

---
+++ src/test/java/com/digitalshop/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalshop.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
